647052fc753fd66e524c31aa557a8dd24108696a
SaynarahNabuco <saynarah@edu.unifor.br>
2024-12-04 21:44:59 -0300

    dessa vez

---
+++ Concessionária/Main.java
@@ -0,0 +1,5 @@
+public static void Main(String [] args){ 
+    System.out.println("---------------------------------------------")
+    System.out.println("Bem vindo, antes de inicia realize seu login!"); 
+    System.out.println("Digite seu Nome: ")
+}
--- Concessionária/Venda.java
+++ Concessionária/Venda.java
@@ -1,26 +1,15 @@
-public abstract class Venda {
-    private String idVenda;
-    private Cliente cliente;
-    private Veiculo veiculo;
+import java.io.IOException; 
+
+public abstract class Venda implements Veiculo, Cliente {
     private double valorVenda;
 
-    public Venda(String idVenda, Cliente cliente, Veiculo veiculo, double valorVenda) {
-        this.idVenda = idVenda;
-        this.cliente = cliente;
-        this.veiculo = veiculo;
+    public Venda(double valorVenda) {
         this.valorVenda = valorVenda;
     }
-    public abstract void processarPagamento();
-    public abstract void concluirVenda();
-    public String getIdVenda() {
-        return idVenda;
-    }
-    public Cliente getCliente() {
-        return cliente;
-    }
-    public Veiculo getVeiculo() {
-        return veiculo;
-    }
+    public abstract void processarPagamento() throws IOException, SQLException;
+
+    public abstract void concluirVenda() throws IOException;
+
     public double getValorVenda() {
         return valorVenda;
     }
